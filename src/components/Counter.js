6d4530f7c20d904d7ba0bd47f08ebc5621eb6af7
Ömer Faruk Akdoğan <omerfakdogan1@gmail.com>
2024-11-12 17:04:17 +0300

    Redux-Intro

---
+++ src/components/Counter.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import counterReducer from './../redux/reducers/counterReducer';
+
+class Counter extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          {this.props.counter}
+        </h1>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps(state){
+  return {counter:state.counterReducer}
+}
+
+export default connect(mapStateToProps)(Counter);
